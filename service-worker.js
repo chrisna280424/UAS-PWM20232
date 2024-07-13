24c2d84c66cb2011d6d0afae315e68ac975edf5e
chrisna280424 <chrisna.putra23@students.utdi.ac.id>
2024-07-13 21:06:22 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,45 @@
-const CACHE_NAME = "cinema-kalasan-xxi-v1";
+const CACHE_NAME = "cinema-kalasan-xxi-v1"; // Nama cache yang akan digunakan
 const urlsToCache = [
-  "./",
-  "./index.js",
-  "./icons.ico",
-  "./bootstrap.bundle.min.js",
-  "./bootstrap.bundle.min.js.map",
-  "./bootstrap.min.css",
-  "./bootstrap.min.css.map",
+  "./", // Halaman utama
+  "./index.js", // Script JavaScript utama
+  "./icons.ico", // Ikon
+  "./bootstrap.bundle.min.js", // Library Bootstrap JS
+  "./bootstrap.bundle.min.js.map", // Source map untuk Bootstrap JS
+  "./bootstrap.min.css", // CSS Bootstrap
+  "./bootstrap.min.css.map", // Source map untuk CSS Bootstrap
 ];
 
 self.addEventListener("install", function (event) {
-  // Perform install steps
+  // Saat Service Worker diinstal
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
+      console.log("Opened cache"); // Logging bahwa cache telah dibuka
+      return cache.addAll(urlsToCache); // Menyimpan semua URL ke dalam cache
     })
   );
 });
 
 self.addEventListener("fetch", function (event) {
+  // Saat ada permintaan network
   event.respondWith(
     caches.match(event.request).then(function (response) {
-      // Cache hit - return response
+      // Mengecek apakah permintaan ada di cache
       if (response) {
-        return response;
+        return response; // Jika ada, mengembalikan respon dari cache
       }
-      return fetch(event.request);
+      return fetch(event.request); // Jika tidak, melanjutkan permintaan ke network
     })
   );
 });
 
 self.addEventListener("activate", function (event) {
-  var cacheWhitelist = ["cinema-kalasan-xxi-v1"];
+  var cacheWhitelist = ["cinema-kalasan-xxi-v1"]; // Daftar cache yang diizinkan
 
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
       return Promise.all(
         cacheNames.map(function (cacheName) {
+          // Menghapus cache yang tidak ada di whitelist
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
